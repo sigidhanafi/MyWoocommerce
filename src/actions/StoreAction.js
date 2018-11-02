acc2caa6d12ff0d9d64388c1b6fb42035091b57f
sigidhanafi@gmail.com <sigidhanafi@gmail.com>
2018-11-02 22:10:35 +0700

    add axios

--- src/actions/StoreAction.js
+++ src/actions/StoreAction.js
@@ -1,10 +1,10 @@
 import { combineEpics, ofType } from 'redux-observable'
-import { mapTo, mergeMap } from 'rxjs/operators'
-import { Observable } from 'rx'
+import { mergeMap } from 'rxjs/operators'
+import axios from 'axios'
 
-const STORE_REQUEST = 'STORE_REQUEST'
-const STORE_REQUEST_SUCCESS = 'STORE_REQUEST_SUCCESS'
-const STORE_REQUEST_FAILED = 'STORE_REQUEST_FAILED'
+export const STORE_REQUEST = 'STORE_REQUEST'
+export const STORE_REQUEST_SUCCESS = 'STORE_REQUEST_SUCCESS'
+export const STORE_REQUEST_FAILED = 'STORE_REQUEST_FAILED'
 
 export const fetchData = () => {
   return {
@@ -12,18 +12,21 @@ export const fetchData = () => {
   }
 }
 
-// export const fetchDataEpic = action$ =>
-//   action$.pipe(
-//     ofType(STORE_REQUEST),
-//     mergeMap(async () => {
-//       await new Promise(resolve => setTimeout(resolve, 3000))
-//       return { type: STORE_REQUEST_SUCCESS }
-//     })
-//   )
-
 export const fetchDataEpic = action$ =>
-  action$.ofType(STORE_REQUEST).mergeMap(() => {
-    return { type: STORE_REQUEST_SUCCESS }
-  })
+  action$.pipe(
+    ofType(STORE_REQUEST),
+    mergeMap(async () => {
+      return await axios
+        .get('http://ubux.biz/test/get-all-stores')
+        .then(response => {
+          const {
+            data: { stores }
+          } = response
+          console.log('haha', stores)
+          return { type: STORE_REQUEST_SUCCESS, data: stores }
+        })
+      // await new Promise(resolve => setTimeout(resolve, 3000))
+    })
+  )
 
 export const combinedStoreEpic = combineEpics(fetchDataEpic)
--- src/reducers/StoreReducer.js
+++ src/reducers/StoreReducer.js
@@ -1,3 +1,5 @@
+import { STORE_REQUEST_SUCCESS } from '../actions/StoreAction'
+
 const initialStore = {
   data: [],
   isLoading: false
@@ -5,9 +7,8 @@ const initialStore = {
 
 export const stores = (state = initialStore, action) => {
   switch (action.type) {
-    case action.type === 'HALLO':
-      return { ...state }
-      break
+    case STORE_REQUEST_SUCCESS:
+      return { ...state, data: action.data }
     default:
       return state
   }
