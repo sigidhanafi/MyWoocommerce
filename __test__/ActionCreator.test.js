d296effcb35b5579d2387a66cd4aec89d85e6114
sigidhanafi@gmail.com <sigidhanafi@gmail.com>
2018-11-03 23:00:54 +0700

    test action creator

--- __test__/ActionCreator.test.js
+++ __test__/ActionCreator.test.js
@@ -1,5 +1,23 @@
-import { fetchData, searchStoreData } from '../src/actions/StoreAction'
-import { STORE_REQUEST, STORE_SEARCH_REQUEST } from '../src/actions/StoreAction'
+import {
+  fetchData,
+  searchStoreData,
+  STORE_REQUEST,
+  STORE_SEARCH_REQUEST
+} from '../src/actions/StoreAction'
+import {
+  fetchDataStoreDetail,
+  STORE_DETAIL_REQUEST
+} from '../src/actions/StoreDetailAction'
+import {
+  fetchDataStoreProduct,
+  STORE_PRODUCT_REQUEST
+} from '../src/actions/StoreProductAction'
+import {
+  selectProduct,
+  deselectProduct,
+  SELECT_PRODUCT,
+  DESELECT_PRODUCT
+} from '../src/actions/SelectedProductAction'
 
 describe('actions', () => {
   it('test - action fetch data', () => {
@@ -15,5 +33,35 @@ describe('actions', () => {
       keyword
     }
     expect(searchStoreData(keyword)).toEqual(expectedAction)
+  })
+  it('test - action fetchDataStoreDetail', () => {
+    const expectedAction = {
+      type: STORE_DETAIL_REQUEST,
+      storeId: '12345'
+    }
+    expect(fetchDataStoreDetail('12345')).toEqual(expectedAction)
+  })
+  it('test - action fetchDataStoreProduct', () => {
+    const expectedAction = {
+      type: STORE_PRODUCT_REQUEST,
+      storeId: '12345'
+    }
+    expect(fetchDataStoreProduct('12345')).toEqual(expectedAction)
+  })
+  it('test - action selectProduct', () => {
+    const product = { id: '1', name: 'product name' }
+    const expectedAction = {
+      type: SELECT_PRODUCT,
+      product
+    }
+    expect(selectProduct(product)).toEqual(expectedAction)
+  })
+  it('test - action deselectProduct', () => {
+    const product = { id: '1', name: 'product name' }
+    const expectedAction = {
+      type: DESELECT_PRODUCT,
+      product
+    }
+    expect(deselectProduct(product)).toEqual(expectedAction)
   })
 })
